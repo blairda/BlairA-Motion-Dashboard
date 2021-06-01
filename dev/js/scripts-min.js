f2845b7379e7ff339ab5f75f7d7c0ac218553b4f
blairda <bda34@drexel.edu>
2021-06-01 15:52:08 -0400

    Actual file setup-blank

---
+++ dev/js/scripts-min.js
@@ -0,0 +1,1 @@
+import{gsap}from"gsap";import{GSDevTools}from"gsap/GSDevTools";gsap.registerPlugin(GSDevTools),GSDevTools.create();let mainTL=gsap.timeline({paused:!0});mainTL.add(moveInTL),mainTL.play();
