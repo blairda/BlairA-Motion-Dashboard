e46f996c45b9b104bf7999c19df28657346eb8fb
blairda <bda34@drexel.edu>
2021-06-07 20:26:34 -0400

    Start logo anim

--- dev/js/startLogos.js
+++ dev/js/startLogos.js
@@ -3,5 +3,11 @@ import { gsap } from "gsap";
 export let startLogosTL = gsap.timeline();
 
 
-startLogosTL.from("#disney-1", {duration: 1, ease: "bounce.out", y: "-=100", alpha: 0})
+startLogosTL
+.from("#disney-1", {duration: 1, ease: "bounce.out", y: "-=75", alpha: 0})
+.from("#pixar-p", {duration: 0.5, ease: "power1.out", y: "+=75", alpha: 0}, "-=0.25")
+.from("#pixar-i", {duration: 0.5, ease: "power1.out", y: "-=75", alpha: 0}, "-=0.25")
+.from("#pixar-x", {duration: 0.5, ease: "power1.out", y: "+=75", alpha: 0}, "-=0.25")
+.from(".pixar-a", {duration: 0.5, ease: "power1.out", y: "-=75", alpha: 0}, "-=0.25")
+.from(".pixar-r", {duration: 0.5, ease: "power1.out", y: "+=75", alpha: 0}, "-=0.25")
 ;
