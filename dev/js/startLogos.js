f7397855dd0248abcf0ac9bca7b2fd105362b82c
blairda <bda34@drexel.edu>
2021-06-07 22:07:52 -0400

    Continue logos

--- dev/js/startLogos.js
+++ dev/js/startLogos.js
@@ -1,4 +1,7 @@
 import { gsap } from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+
+gsap.registerPlugin(DrawSVGPlugin);
 
 export let startLogosTL = gsap.timeline();
 
@@ -10,4 +13,9 @@ startLogosTL
 .from("#pixar-x", {duration: 0.5, ease: "power1.out", y: "+=75", alpha: 0}, "-=0.25")
 .from(".pixar-a", {duration: 0.5, ease: "power1.out", y: "-=75", alpha: 0}, "-=0.25")
 .from(".pixar-r", {duration: 0.5, ease: "power1.out", y: "+=75", alpha: 0}, "-=0.25")
+
+.to("#logos-1", {duration:0.5, alpha: 0, delay:0.5})
+.to("#logo-box-1", {duration:0.75, y:"+=100"})
+.fromTo("#sig-line-1", {drawSVG:"0% 0%"},{duration: 0.75, drawSVG:"0% 100%", ease: "power1.out"}, "box-out")
+.to("#logo-box-1", {duration:0.25, alpha: 0}, "box-out")
 ;
